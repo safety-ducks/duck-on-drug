249ba9e79982cb6097689788e9b30730ee8b368d
Maksymilian Stachowiak <mstachowiak19@edu.cdv.pl>
2019-10-26 06:43:20 +0200

    Enemy

---
+++ Enemy.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var enemy_speed = 1
+export var time_min = 3
+export var time_max = 5
+var Velocity = Vector2()
+
+func flip():
+	Velocity.x *= -1
+	$AnimatedSprite.flip_h = Velocity.x < 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Velocity.x = enemy_speed
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	if not $BottomFront.is_colliding() or not $BottomRear.is_colliding() or $Rear.is_colliding() or $Front.is_colliding(): 
+		flip()
+		pass
+	
+	position.x += Velocity.x*delta
+
+		
+func _on_Timer_timeout():
+	flip()
+	#$Timer.stop()
+	$Timer.wait_time = rand_range(2,5)
+	pass # Replace with function body.
--- scenses/BasicEnemy.gd
+++ scenses/BasicEnemy.gd
@@ -9,6 +9,10 @@ export var time_min = 3
 export var time_max = 5
 var Velocity = Vector2()
 
+func flip():
+	Velocity.x *= -1
+	$Area2D/Sprite.flip_h = Velocity.x < 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Velocity.x = enemy_speed
@@ -20,14 +24,14 @@ func _ready():
 
 func _physics_process(delta):
 	if not $RayCast2D.is_colliding() or not $RayCast2D2.is_colliding() or $RayCast2D3.is_colliding() or $RayCast2D4.is_colliding():
-		Velocity.x *= -1
+		flip()
 		pass
 	
 	position.x += Velocity.x*delta
 
 		
 func _on_Timer_timeout():
-	Velocity.x *= -1
+	flip()
 	#$Timer.stop()
 	$Timer.wait_time = rand_range(2,5)
 	pass # Replace with function body.
